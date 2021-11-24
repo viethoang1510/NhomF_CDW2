d588c6e42bce02b0fca2aafd89a9cad7364e3b84
Việt Hoàng <tieutam1510@gmail.com>
2021-11-25 00:05:52 +0700

    Update CN-Slide by Hoang

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace App\Http\Controllers\Auth;
+ 
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+ 
+class LogoutController extends Controller
+{
+    public function __construct() {
+    	$this->middleware('auth');
+    }
+		
+	public function getLogout() {
+		Auth::logout();
+		return redirect('login');
+	}
+}
