7c483075b248927375cd9b923c59176914fa62fa
viethoang1510 <90826691+viethoang1510@users.noreply.github.com>
2021-11-25 10:58:53 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,6 @@
 @section('content')
  <!-- slide trình chiếu -->
  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to=""></li>
-        </ul>
             @foreach($slides as $key => $item)
             <div class="carousel-item {{ $key == 1 ? 'active' : '' }} ">
                 <img class="d-block w-100" src="public/upload/{{ $item->slide1 }}">
